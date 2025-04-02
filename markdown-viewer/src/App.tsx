8fadc04835a7aededdcb9c072f47d29f1305f9c6
mraduarte <mcduarte9@hotmail.com>
2025-04-01 21:07:41 -0300

    Criado projeto

---
+++ markdown-viewer/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState, useEffect, useRef } from "react"
+import Toolbar from "./components/Toolbar"
+
+function App() {
+
+  return (
+    <div className="app-container">
+      <Toolbar></Toolbar>
+      <textarea></textarea>
+      <div></div>
+    </div>
+
+  )
+}
+
+export default App
